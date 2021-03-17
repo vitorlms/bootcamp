06985d421d56b62ed77aebde6fef14f05dc77c3c
Vitor Mourato <vlms@cin.ufpe.br>
2021-03-17 10:47:36 -0300

    expanding Node.js project
    Nodemon, HTTP methods(and Insomnia), middlewares and functioning simple local server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,87 @@
+const {request} = require('express');
 const express = require('express');
-
+const {uuid, isUuid} = require('uuidv4');
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello world!'});
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next) {
+    const {method, url} = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.time(logLabel);
+
+    next();
+
+    console.timeEnd(logLabel)
+};
+
+function validateProjectId(request, response, next) {
+    const {id} = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({error: 'invalid project ID'});
+    };
+
+    return next();
+};
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId)
+
+app.get('/projects', (request, response) => {
+    const {title} = request.query;
+    
+
+    // what is this I just wrote?
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    const {title, owner} = request.body;
+    const project = {id: uuid(), title, owner};
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put('/projects/:id', validateProjectId, (request, response) => {
+    const {id} = request.params;
+    const {title, owner} = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({error: 'project not found'});
+    };
+
+    const project = {id, title, owner};
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
+app.delete('/projects/:id', validateProjectId, (request, response) => {
+    const {id} = request.params;
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({error: 'project not found'});
+    };
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
+});
+
+app.listen(3333, () => {
+    console.log('Back-end started!');
+});
